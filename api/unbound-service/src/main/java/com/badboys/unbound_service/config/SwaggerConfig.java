7b5bb6581d23c88869e0100a54677b608512d6d8
bshello25 <bshello25@gmail.com>
2025-02-08 14:51:12 +0900

    스웨거 설정 매핑오류 수정

--- api/unbound-service/src/main/java/com/badboys/unbound_service/config/SwaggerConfig.java
+++ api/unbound-service/src/main/java/com/badboys/unbound_service/config/SwaggerConfig.java
@@ -1,16 +1,35 @@
 package com.badboys.unbound_service.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${host}")
+    private String hostUrl;
+
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
                 .group("unbound-public")
                 .pathsToMatch("/**") // API 경로 지정
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Unbound API")
+                        .version("1.0")
+                        .description("Unbound 서비스의 API 문서입니다.")
+                )
+                .addServersItem(new Server().url(hostUrl).description("Gateway Server")); // ✅ Gateway URL 설정
     }
 }
