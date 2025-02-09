d18b3052ad7f907bf1b7bebccda7226a28bc81e7
bshello25 <bshello25@gmail.com>
2025-02-10 00:06:41 +0900

    누락 코드 수정

--- api/unbound-service/src/main/java/com/badboys/unbound_service/api/MatchController.java
+++ api/unbound-service/src/main/java/com/badboys/unbound_service/api/MatchController.java
@@ -4,6 +4,8 @@ import com.badboys.unbound_service.api.service.MatchService;
 import com.badboys.unbound_service.model.RequestUpdateCommentDto;
 import com.badboys.unbound_service.model.ResponseMatchInfoDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -74,7 +76,8 @@ public class MatchController {
 
     @Operation(summary = "매칭 기록 정보 조회", description = "매칭 기록 상세 페이지")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "200", description = "조회 성공",
+                    content = @Content(schema = @Schema(implementation = ResponseMatchInfoDto.class))),
             @ApiResponse(responseCode = "404", description = "매치 없음"),
             @ApiResponse(responseCode = "500", description = "서버 에러")
     })
