d405e5abae170fe750e50d13a95a80460b8c8e26
bshello25 <bshello25@gmail.com>
2025-06-30 03:09:45 +0900

    초대 메세지 카프카 컨슈머 누락설정 수정

--- api/unbound-chat/src/main/java/com/badboys/unbound_chat/config/InviteConsumerConfig.java
+++ api/unbound-chat/src/main/java/com/badboys/unbound_chat/config/InviteConsumerConfig.java
@@ -4,6 +4,7 @@ import com.badboys.unbound_chat.api.model.RequestInviteDto;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -11,6 +12,7 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@Configuration
 public class InviteConsumerConfig {
 
     @Value("${spring.kafka.bootstrap-servers}")
