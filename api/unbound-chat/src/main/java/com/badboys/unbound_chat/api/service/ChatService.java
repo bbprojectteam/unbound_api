f1317949b253fd9e95bf4112ae2ae64f8e1563c8
bshello25 <bshello25@gmail.com>
2025-07-06 20:19:39 +0900

    챗 메시지 아이디 할당 에러 수정

--- api/unbound-chat/src/main/java/com/badboys/unbound_chat/api/service/ChatService.java
+++ api/unbound-chat/src/main/java/com/badboys/unbound_chat/api/service/ChatService.java
@@ -126,12 +126,12 @@ public class ChatService {
                 ? "📷 사진을 보냈습니다."
                 : chatMessage.getMessage();
 
-        chatMessage.setChatMessageId(Long.valueOf(chatMessageDocument.getId()));
+        chatMessage.setChatMessageId(nextMessageId);
 
         try {
             // WebSocket을 통해 메시지 브로드캐스트
-            messagingTemplate.convertAndSend("/topic/chat/" + chatMessage.getChatRoomId(), chatMessage,
-                    messageHeaders(chatMessage.getSenderId().toString()));
+//            messagingTemplate.convertAndSend("/topic/chat/" + chatMessage.getChatRoomId(), chatMessage,
+//                    messageHeaders(chatMessage.getSenderId().toString()));
 
             log.info("WebSocket으로 메시지 전송 완료: {}", chatMessage);
         } catch (Exception e) {
